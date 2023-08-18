6b873310169e4b2bbe90618a843666f9b090dc1f
Marc Schwieterman <marc@marcisme.com>
2023-08-18 08:40:34 -0700

    Update demo for daily-ios 0.10.0

--- DailyDemo/View Controllers/ParticipantViewController.swift
+++ DailyDemo/View Controllers/ParticipantViewController.swift
@@ -112,8 +112,8 @@ class ParticipantViewController: UIViewController {
                 .activeRemote: .set(
                     profile: .set(.base)
                 ),
-            ])
-        )
+            ]),
+            completion: nil)
     }
 }
 
